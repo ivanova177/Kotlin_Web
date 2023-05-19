7c45713d59b69115429faf780b5f4cd4b4655e79
olenk <sisi-fox@mail.ri>
2023-05-19 22:02:23 +0700

    Lab_1.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,128 @@
-fun main(args: Array<String>) {
-    val mobilePhone = MobilePhone()
+class Node(var key: Int) {
+    var left: Node? = null
+    var right: Node? = null
+}
 
-    println("\nНовый контакт: ")
-    mobilePhone.addNewContact(Contact("Татьяна", "+7-909-567-34-23"))
-    mobilePhone.addNewContact(Contact("Олег", "+7-954-945-36-43"))
-    mobilePhone.addNewContact(Contact("Наталья", "+7-955-909-12-21"))
-    mobilePhone.addNewContact(Contact("Петр", "+7-999-683-00-00"))
-    mobilePhone.printContacts()
+class BinaryTree {
+    private var root: Node? = null
 
-    println("\nРедактирование контакта: ")
-    mobilePhone.updateContact(Contact("Татьяна", "+7-909-567-34-23"), Contact("Татьяна", "+7-999-657-33-23"))
-    mobilePhone.printContacts()
-
-    println("\nУдаление: ")
-    mobilePhone.removeContact(Contact("Олег", "+7-954-945-36-43"))
-    println("Контакт удален ")
-    mobilePhone.printContacts()
-
-    println("\nПоиск контакта Contact: ")
-    val status = mobilePhone.findContact(Contact("Наталья", "+7-955-909-12-21"))
-    if (status >= 0) {
-        println("Контакт существует")
-    }
-    else {
-        println("Контакт не существует")
+    fun insert(key: Int) {
+        root = insertRecursive(root, key)
     }
 
-    println("\nПоиск контакта по имени : ")
-    val foundContact = mobilePhone.queryContact("Петр")
-    if (foundContact != null) {
-        println("${foundContact.name} " +
-                "\n${foundContact.phoneNumber}")
+    private fun insertRecursive(current: Node?, key: Int): Node {
+        if (current == null) {
+            return Node(key)
+        }
+
+        if (key < current.key) {
+            current.left = insertRecursive(current.left, key)
+        } else if (key > current.key) {
+            current.right = insertRecursive(current.right, key)
+        }
+
+        return current
     }
+
+    fun search(key: Int): Boolean {
+        return searchRecursive(root, key)
+    }
+
+    private fun searchRecursive(current: Node?, key: Int): Boolean {
+        if (current == null) {
+            return false
+        }
+
+        if (key == current.key) {
+            return true
+        }
+
+        return if (key < current.key) {
+            searchRecursive(current.left, key)
+        } else {
+            searchRecursive(current.right, key)
+        }
+    }
+
+    fun delete(key: Int) {
+        root = deleteRecursive(root, key)
+    }
+
+    private fun deleteRecursive(current: Node?, key: Int): Node? {
+        if (current == null) {
+            return null
+        }
+
+        if (key == current.key) {
+            if (current.left == null && current.right == null) {
+                return null
+            }
+
+            if (current.right == null) {
+                return current.left
+            }
+
+            if (current.left == null) {
+                return current.right
+            }
+
+            val smallestValue = findSmallestValue(current.right)
+            current.key = smallestValue
+            current.right = deleteRecursive(current.right, smallestValue)
+            return current
+        }
+
+        if (key < current.key) {
+            current.left = deleteRecursive(current.left, key)
+        } else {
+            current.right = deleteRecursive(current.right, key)
+        }
+
+        return current
+    }
+
+    private fun findSmallestValue(root: Node?): Int {
+        return if (root?.left == null) {
+            root!!.key
+        } else {
+            findSmallestValue(root.left)
+        }
+    }
+
+    fun inorderTraversal() {
+        inorderTraversalRecursive(root)
+    }
+
+    private fun inorderTraversalRecursive(node: Node?) {
+        if (node != null) {
+            inorderTraversalRecursive(node.left)
+            println(node.key)
+            inorderTraversalRecursive(node.right)
+        }
+    }
+}
+
+fun main() {
+    val tree = BinaryTree()
+
+    // Вставка элементов в дерево
+    tree.insert(50)
+    tree.insert(30)
+    tree.insert(20)
+    tree.insert(40)
+    tree.insert(70)
+    tree.insert(60)
+    tree.insert(80)
+
+    // Поиск элемента в дереве
+    println(tree.search(60)) // Вывод: true
+    println(tree.search(90)) // Вывод: false
+
+    // Удаление элемента из дерева
+    tree.delete(20)
+    tree.delete(30)
+
+    // Обход дерева
+    println("Обход дерева:")
+    tree.inorderTraversal()
 }
